fcffe50f88bc38023d1c02897747f5d7e3e16997
Igor <igorblosgressler.com>
2020-08-07 10:55:51 -0300

    Criação de funções Get/Set na conta, nova classe cliente

--- conta.py
+++ conta.py
@@ -7,12 +7,39 @@ class ContaCorrente:
         self.__limite = limte
 
     def exibir_extrato(self):
-        print("Extrado do titulaar {0}:{1}".format(self.titular ,self.saldo))
+        print("Extrado do titulaar {0}:{1}".format(self.__titular, self.__saldo))
 
-    def depositar(self, valor):
-        self.saldo += valor
-        self.exibir_extrato()
+    def depositar(self, valor, exibir_extrato=False):
+        self.__saldo += valor
+        if(exibir_extrato):
+            self.exibir_extrato()
 
-    def sacar(self, valor):
-        self.saldo -= valor
-        self.exibir_extrato()
+    def __pode_sacar(self, valor_saque):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_saque <= (valor_disponivel)
+
+    def sacar(self, valor, exibir_extrato=False):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            if(exibir_extrato):
+                self.exibir_extrato()
+        else:
+            print("O valor {0} passou o limite".format(valor))
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
